6b781ab7d3ff8115ceec5e5b07b304720ec3f5dc
suyashmadhesia <suyashmadhesia@gmail.com>
2021-11-01 21:58:24 +0530

    refraction

--- test/block.test.js
+++ test/block.test.js
@@ -1,6 +1,6 @@
-const Block = require("./block");
-const { GENESIS_DATA } = require("./config");
-const cryptoHash = require("./crypto-hash");
+const Block = require("../block");
+const { GENESIS_DATA } = require("../config");
+const cryptoHash = require("../crypto-hash");
 
 describe("Block", () => {
   const timestamp = "a-date";
